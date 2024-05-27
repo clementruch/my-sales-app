2fca6bb93f191f62ad4c7b5142423280658c9bb8
clementruch <144322316+clementruch@users.noreply.github.com>
2024-05-27 19:27:31 +0200

    jusqu'a 4.13

---
+++ src/app/categories/category.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Category } from './category.dto';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http: HttpClient) { }
+  
+  public getAll(): Observable<Category[]> {
+    return this.http.get<Category[]>(environment.api + 'categories');
+  }
+}
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    api: "http://monsupersite.com:5461/api/",
+};
